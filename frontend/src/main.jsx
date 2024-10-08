cc54444a6c15c08091879853d6dbe60d3c92f51e
kalivancleave <kalivancleave@gmail.com>
2024-10-08 04:20:28 -0700

    login modal created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
 import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
-  
+
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
@@ -20,8 +20,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
