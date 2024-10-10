075b9a8a6e1c1d71014f38676efbabaa38649a02
kalivancleave <kalivancleave@gmail.com>
2024-10-10 02:33:08 -0700

    removed the extra test routes in routes/api/index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,21 +7,24 @@ router.use(restoreUser); //make sure this happens first ALWAYS
 
 //login & logout
 router.use('/session', sessionRouter);
+
 //signup
 router.use('/users', usersRouter);
 //add new routes here (reservations, pets, etc.)
 
+
+router.get('/test', requireAuth, (req, res, next) => {
+  try {
+    res.json({
+      message: ':)'
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body })
 });
-
-
-// GET /api/restore-user
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
 
 module.exports = router;
