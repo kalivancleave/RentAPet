85f8eb7799cef1c7a46f3e05c30c1f617dba0b26
kalivancleave <kalivancleave@gmail.com>
2024-10-10 07:12:07 -0700

    more updates for the animals api route

--- backend/routes/api/animals.js
+++ backend/routes/api/animals.js
@@ -10,6 +10,31 @@ const { where } = require('sequelize');
 
 const router = express.Router();
 
+//validate spot info
+const validateAnimal = [
+  check('name')
+    .exists({checkFalsy: true})
+    .isString()
+    .isLength({
+      min: 1,
+      max: 49
+    })
+    .withMessage('Name is required'),
+  check('birthday')
+    .exists({checkFalsy: true})
+    .withMessage('birthday is required'),
+  check('type')
+    .exists({checkFalsy: true})
+    .withMessage('type is required'),
+  check('price')
+    .exists({checkFalsy: true})
+    .withMessage('Price is required'),
+  check('price')
+    .isFloat({min: 0})
+    .withMessage('Price must be a positive number'),
+  handleValidationErrors
+];
+
 //get all animals
 router.get('/', async(req, res, next) => {
   try {
@@ -155,6 +180,184 @@ router.get('/:animalId', async(req, res, next) => {
 
     res.json(updatedAnimal)
     
+  } catch (error) {
+    next(error)
+  }
+})
+
+//get all reviews for an animal
+router.get('/:animalId/reviews', async (req, res, next) => {
+  try {
+    //find animal id
+    const animalId = req.params.animalId
+
+    //find animal by id
+    const animal = await Animal.findByPk(animalId)
+
+    //404 - no animal found
+    if(!animal){
+      res.status(404)
+      return res.json({
+        message: "Animal couldn't be found"
+      })
+    };
+
+    //find reviews by id
+    const reviews = await Review.findAll({
+      where: {
+        animalId: animal.id
+      }
+    });
+
+    //iterate through reviews
+    let updatedReviews = []
+    for(let i = 0; i < reviews.length; i++){
+      let review = reviews[i]
+
+      //find user
+      const user = await User.findOne({
+        where: {
+          id: review.userId
+        },
+        attributes: ['id', 'firstName', 'lastName']
+      });
+
+      const payload = {
+        id: review.id,
+        userId: review.userId,
+        animalId: review.animalId,
+        review: review.review,
+        stars: review.stars,
+        createdAt: review.createdAt,
+        updatedAt: review.updatedAt,
+        User: user
+      }
+      updatedReviews.push(payload)
+    }
+
+    //return requested response
+    res.json({
+      Reviews: updatedReviews
+    })
+    
+  } catch (error) {
+    next(error)
+  }
+});
+
+//get reservations for an animal based on the animal id
+router.get('/:animalId/reservations', requireAuth, async(req, res, next) => {
+  try {
+    //find animal id
+    const animalId = req.params.animalId;
+
+    //find animal by id
+    const animal = await Animal.findByPk(animalId);
+
+    //404 - no animal found
+    if(!animal) {
+      res.status(404)
+      return res.json({
+        message: "Animal couldn't be found"
+      })
+    };
+
+    //animal found
+    //find all reservations based on animal id
+    const reservations = await Reservation.findAll({
+      where: {
+        animalId: animalId
+      }
+    });
+
+    //sort between reservations if the user id is the animal owner
+    //or if they are not the owner (animal owner will see more info than non-owner)
+    let ownerReservations = [];
+    let nonOwnerReservations = [];
+    if(animal.userId === req.user.id) {
+      for (let i = 0; i < reservations.length; i++){
+        let reservation = reservations[i];
+
+        const user = await User.findOne({
+          where: {
+            id: reservation.userId
+          },
+          attributes: ['id', 'firstName', 'lastName']
+        });
+
+        const ownerReservationInfo = {
+          User: user,
+          id: reservation.id,
+          animalId: reservation.animalId,
+          userId: reservation.userId,
+          startDate: reservation.startDate.toISOString().split('T')[0],
+          endDate: reservation.endDate.toISOString().split('T')[0],
+          createdAt: reservation.createdAt,
+          updatedAt: reservation.updatedAt
+        }
+
+        ownerReservations.push(ownerReservationInfo);
+      }
+    } else {
+      for (let i = 0; i < reservations.length; i++) {
+        let reservation = reservations[i];
+
+        const nonOwnerReservationInfo = {
+          animalId: reservation.animalId,
+          startDate: reservation.startDate.toISOString().split('T')[0],
+          endDate: reservation.endDate.toISOString().split('T')[0]
+        }
+
+        nonOwnerReservations.push(nonOwnerReservationInfo);
+      }
+    };
+
+    //if user.id is animal owner id
+    if(animal.userId === req.user.id){
+      return res.json({
+        Reservations: ownerReservations
+      });
+    }
+
+    //response for non owner
+    res.json({
+      Reservations: nonOwnerReservations
+    })
+    
+  } catch (error) {
+    next(error)
+  }
+});
+
+//create an animal
+router.post('/', requireAuth, validateAnimal, async(req, res, next) => {
+  try {
+    //find the id of the logged in user
+    const userId = req.user.id;
+
+    //descructure from body
+    const {name, birthday, type, price} = req.body
+
+    //create a new animal
+    const newAnimal = await Animal.create({
+      userId: userId,
+      name,
+      birthday,
+      type,
+      price
+    });
+
+    //return requested response
+    res.status(201),
+    res.json({
+      is: parseInt(newAnimal.id),
+      userId: parseInt(newAnimal.userId),
+      name: newAnimal.name,
+      birthday: newAnimal.birthday,
+      type: newAnimal.type,
+      price: parseFloat(newAnimal.price)
+    })
+
   } catch (error) {
     next(error)
   }
