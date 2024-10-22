027fadab465aefb8b066016af1236aae709d701d
kalivancleave <kalivancleave@gmail.com>
2024-10-22 11:37:01 -0700

    update and delete review styling done

--- frontend/src/components/Reviews/UpdateReviewModal.jsx
+++ frontend/src/components/Reviews/UpdateReviewModal.jsx
@@ -7,11 +7,14 @@ import { fetchAnimalReviews } from "../../store/reviews";
 
 import { IoPawSharp } from "react-icons/io5";
 
+import Logo from '../../../../static/rentAPetLogoDark.png';
+
 const UpdateReviewModal = (props) => {
   const reviewId = props.reviewId
   const dispatch = useDispatch();
   const allReviews = useSelector(state => state?.reviews.reviews)
   let currentReview = allReviews?.filter(review => review.id === reviewId);
+  const currentAnimal = useSelector(state => state.animals.animalDetails)
   
   const [uReview, setReview] = useState(currentReview[0].review);
   const [uStars, setStars] = useState(currentReview[0].stars);
@@ -61,24 +64,41 @@ const UpdateReviewModal = (props) => {
   }
 
   return (
-    <>
-      <h1>Update Review Modal</h1>
-      {console.log(currentReview[0].id)}
-      <p>Update your review</p>
-      <form onSubmit={handleUpdate}>
-        <textarea
-          placeholder="Please leave your review here..."
-          onChange={(e) => setReview(e.target.value)}
-          value={uReview}
-        />
-        {errors.review && <p>{errors.review}</p>}
+    <div className='displayFlex flexColumn alignCenter'>
+      <img className='smallLogo' src={Logo} />
 
-        <div>
+      <div className="displayFlex flexColumn alignCenter bottomPadding topPadding">
+        <p className='header xx-largeFont noMargin almostBlackFont'>Update Review</p>
+        <img className="imageShape" src={currentAnimal?.animalImage} />
+      </div>
+
+        <form className='displayFlex flexColumn littleMoreTopPadding' onSubmit={handleUpdate}>
+
+        <div className='displayFlex alignCenter topPadding fullWidth spaceBetween alignTop'>
+            <label className='largeFont displayFlex font almostBlackFont'>
+              Thoughts
+            </label>
+
+          <textarea
+            placeholder="Please leave your review here..."
+            onChange={(e) => setReview(e.target.value)}
+            className='noBorder dropShadow textAreaSize littleMoreLeftMargin'
+            value={uReview}
+          />
+          {errors.review && <p>{errors.review}</p>}
+        </div>
+
+
+        <div className='displayFlex alignCenter topPadding fullWidth spaceEvenly'>
+          <label className='largeFont displayFlex font almostBlackFont'>
+            Stars
+          </label>
+            
           {starImages.map((_, index) => {
             return (
               <IoPawSharp 
                 key={index}
-                className="xlargeFont cursorPointer"
+                className="xx-largeFont cursorPointer"
                 color={(hoverValue || uStars) > index ? colors.green : colors.grey}
                 onClick={() => handleClick(index +1)}
                 onMouseOver={() => handleMouseOver(index + 1)}
@@ -87,14 +107,16 @@ const UpdateReviewModal = (props) => {
               />
             )
           })}
-          <label>Stars</label>
-        </div>
         {errors.stars && <p>{errors.stars}</p>}
+        </div>
 
-        <button onClick={handleUpdate}>Update</button>
+        <div className="fullWidth textCenter topMargin">
+          <button onClick={handleUpdate}>Update</button>
+        </div>
+
       </form>
 
-    </>
+    </div>
   )
 }
 
